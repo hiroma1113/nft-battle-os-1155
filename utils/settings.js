31e116addc3288656d44396e66c35f119b86980c
hiroma1113 <114849344+hiroma1113@users.noreply.github.com>
2022-10-02 16:05:35 +0900

    Update settings.js

--- utils/settings.js
+++ utils/settings.js
@@ -1,9 +1,10 @@
 const siteTitle = 'NFT バトル'
 
-const detectedTraits = ['attack', 'defense', 'dexterity', 'hp']
+const detectedTraits = ['attack', 'defense', 'hp']
 
 const detectedTokenIds = [
   '73945963254904917110958959709365494941141689289120657829957101337116810412033',
   '73945963254904917110958959709365494941141689289120657829957101332718763900929'
 ]
+
 export { siteTitle, detectedTraits, detectedTokenIds }
